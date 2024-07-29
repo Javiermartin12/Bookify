39289ff3775fc476e27c6a6fa50fe4da453ad616
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-29 11:51:04 +0200

    Chore: imported sideBAR

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import "./CSS/App.css";
-
-function App() {
+import React from "react";
+import SideBar from "./components/SideBar.tsx";
+const App: React.FC = () => {
   return (
     <>
       <div className="background">
-        <div className="continerMain"></div>
+        <div className="continerMain">
+          <SideBar />
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
