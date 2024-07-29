d6277de81bdbaadfe8a00d886524254fc8f1e4b8
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-29 12:28:35 +0200

    Chore: created and import right container

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import "./CSS/App.css";
 import React from "react";
 import SideBar from "./components/SideBar.tsx";
+import RightContainer from "./components/RightContainer.tsx";
+
 const App: React.FC = () => {
   return (
     <>
       <div className="background">
         <div className="continerMain">
           <SideBar />
+          <RightContainer />
         </div>
       </div>
     </>
---
+++ Frontend/src/components/RightContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../CSS/componentsCSS/rightContainer.css";
+import Header from "./Header";
+
+const RightContainer: React.FC = () => {
+  return (
+    <div className="rightContainer">
+      <Header />
+    </div>
+  );
+};
+
+export default RightContainer;
