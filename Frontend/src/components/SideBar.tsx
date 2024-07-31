e533d28ae7dc24d61766234cb78fe0197e28c406
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-31 11:36:33 +0200

    chore: refactored code

--- Frontend/src/components/SideBar.tsx
+++ Frontend/src/components/SideBar.tsx
@@ -1,9 +1,19 @@
 import "../CSS/componentsCSS/sideBar.css";
 import React from "react";
+import { Home, Message, Search, Add, Favorite } from "@mui/icons-material";
+import { Box } from "@mui/material";
+import { sideBarContainerStyles } from "../theme/materialUI/mtUI";
 
 const SideBar: React.FC = () => {
   return (
     <div className="sideBar">
+      <Box sx={sideBarContainerStyles}>
+        <Home />
+        <Favorite />
+        <Add />
+        <Search />
+        <Message />
+      </Box>
       <h1 className="sideBarTitle">Home</h1>
       <p className="sideBarSubTitle">All books</p>
     </div>
