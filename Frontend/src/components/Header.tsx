bc630118776bfcb0ecc3a01c07632b8cae864659
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-31 13:04:30 +0200

    CHore: code refactored  from styles

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,31 +1,20 @@
 import React from "react";
 import "../CSS/componentsCSS/header.css";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { filterListIcon, headerButtonStyles } from "../theme/materialUI/mtUI";
+import FilterListIcon from "@mui/icons-material/FilterList";
 const Header: React.FC = () => {
   return (
     <div className="headerMain">
       <div className="headerContainer">
-        <h2 className="headerTitle">Books</h2>
-        <p className="booksExist"> Number of books exist</p>
+        <Box>
+          <h2 className="headerTitle">Books</h2>
+          <p className="booksExist"> Number of books exist</p>
+        </Box>
       </div>
       <div className="headerContainerRight">
-        <Button
-          variant="contained"
-          sx={{
-            background: "rgba(0, 0, 0, 0.2)",
-            borderRadius: "0.5rem",
-            overflow: "hidden",
-            marginTop: "0.5rem",
-            backdropFilter: "blur(10px)",
-            WebkitBackdropFilter: "blur(10px)",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-            height: "40%",
-            width: "60%",
-            "&:hover": {
-              background: "rgba(0, 0, 0, 0.3)",
-            },
-          }}
-        >
+        <FilterListIcon sx={filterListIcon} />
+        <Button variant="contained" sx={headerButtonStyles}>
           New book
         </Button>
       </div>
