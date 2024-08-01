ef0ab0efe69bb584c8651b127acc1902092c9c59
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-01 12:54:55 +0200

    Chore: created middleare of user

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { userRoutes } from "./routes/user.routes";
 
 export const app = express();
+//middleware
 app.use(express.json());
+app.use("/user", userRoutes);
