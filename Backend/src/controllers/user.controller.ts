96c3cbf9b8384d61cf2b7738ccc3e0b2cac9367d
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-02 12:36:14 +0200

    Chore: created user post to data bases

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from "express";
+import { UserModel } from "../models/users.models";
 
-export const getAllUser = (req: Request, res: Response) => {
+export const getAllUser = async (req: Request, res: Response) => {
   res.send;
 };
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
+  const { id, name, gmail, password } = req.body;
+  try {
+    const newUser = await UserModel.create({ id, name, gmail, password });
+    res.status(200).send(newUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+export const updateUser = async (req: Request, res: Response) => {
   res.send;
 };
-export const updateUser = (req: Request, res: Response) => {
-  res.send;
-};
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
   res.send;
 };
