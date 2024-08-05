6699f5e2ab84b77053b438ebec887511d5ef23fa
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-05 10:37:20 +0200

    Chore: created books controller, update, get & delete

--- Backend/src/controllers/books.controller.ts
+++ Backend/src/controllers/books.controller.ts
@@ -2,6 +2,12 @@ import { Request, Response } from "express";
 import { BooksModel } from "../models/books.models";
 export const getAllBooks = async (req: Request, res: Response) => {
   console.log(req, res);
+  try {
+    const getBooks = await BooksModel.find();
+    res.status(200).send(getBooks);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 export const createBooks = async (req: Request, res: Response) => {
   const {
@@ -32,9 +38,45 @@ export const createBooks = async (req: Request, res: Response) => {
 };
 
 export const updateBooks = async (req: Request, res: Response) => {
-  console.log(req, res);
+  const {
+    title,
+    author,
+    genre,
+    publishedDate,
+    summary,
+    coverImageUrl,
+    synopsis,
+    idUser,
+  } = req.body;
+  const { userId } = req.params;
+  try {
+    const bookUpdated = await BooksModel.findByIdAndUpdate(
+      { _id: userId },
+      {
+        title,
+        author,
+        genre,
+        publishedDate,
+        summary,
+        coverImageUrl,
+        synopsis,
+        idUser,
+      },
+      { new: true }
+    );
+    res.status(200).send(bookUpdated);
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
 };
 
 export const deleteBooks = async (req: Request, res: Response) => {
-  console.log(req, res);
+  const { userId } = req.params;
+  try {
+    const deletedBook = await BooksModel.findByIdAndDelete({ _id: userId });
+    res.status(200).send(deletedBook);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
