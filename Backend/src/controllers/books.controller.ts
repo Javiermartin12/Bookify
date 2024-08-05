a2863bac23a3a9c7a723796fe7b013871bc02477
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-05 09:44:33 +0200

    Chore: defaultcode

--- Backend/src/controllers/books.controller.ts
+++ Backend/src/controllers/books.controller.ts
@@ -1,35 +1,40 @@
-// import { Request, Response } from "express";
-// import { UserModel } from "../models/users.models";
-// import { BooksModel } from "../models/books.models";
-// export const getAllBooks = async (req: Request, res: Response) => {};
-// export const createBooks = async (req: Request, res: Response) => {
-//   const {
-//     title,
-//     author,
-//     genre,
-//     publishedDate,
-//     summary,
-//     coverImageUrl,
-//     synopsis,
-//     userId,
-//   } = req.body;
-//   try {
-//     const newBook = await BooksModel.create({
-//       title,
-//       author,
-//       genre,
-//       publishedDate,
-//       summary,
-//       coverImageUrl,
-//       synopsis,
-//       userId,
-//     });
-//     res.status(200).send(newBook);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// };
+import { Request, Response } from "express";
+import { BooksModel } from "../models/books.models";
+export const getAllBooks = async (req: Request, res: Response) => {
+  console.log(req, res);
+};
+export const createBooks = async (req: Request, res: Response) => {
+  const {
+    title,
+    author,
+    genre,
+    publishedDate,
+    summary,
+    coverImageUrl,
+    synopsis,
+    userId,
+  } = req.body;
+  try {
+    const newBook = await BooksModel.create({
+      title,
+      author,
+      genre,
+      publishedDate,
+      summary,
+      coverImageUrl,
+      synopsis,
+      userId,
+    });
+    res.status(200).send(newBook);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-// export const updateBooks = async (req: Request, res: Response) => {};
+export const updateBooks = async (req: Request, res: Response) => {
+  console.log(req, res);
+};
 
-// export const deleteBooks = async (req: Request, res: Response) => {};
+export const deleteBooks = async (req: Request, res: Response) => {
+  console.log(req, res);
+};
