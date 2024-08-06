dd145d202e09140b7670f64d6301bfa73a8aa4fe
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-06 13:47:06 +0200

    Chore: configured cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { userRoutes } from "./routes/user.routes";
 import { booksRoutes } from "./routes/books.routes";
+import cors from "cors";
 
 export const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 //middleware
 app.use(express.json());
 app.use("/user", userRoutes);
