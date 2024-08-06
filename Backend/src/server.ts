b5c2fd2dcd8488fe8d93f8dfc212e9f392193435
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-06 14:34:18 +0200

    Chore: added jwtcheck like middleware to encrypt data

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { userRoutes } from "./routes/user.routes";
 import { booksRoutes } from "./routes/books.routes";
 import cors from "cors";
+import { jwtCheck } from "./middleware/check.jwt";
 
 export const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+
 //middleware
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-app.use("/user", userRoutes);
+app.use("/user", jwtCheck, userRoutes);
 app.use("/books", booksRoutes);
