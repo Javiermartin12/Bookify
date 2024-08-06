9bc278effed397755377e015ac0d54c8c57e7d48
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-06 10:30:45 +0200

    Chore: configured auth provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,11 +4,21 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/materialUI/mtUI.ts";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
