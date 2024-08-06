3e39b81127d91de7ada9e663f6e6eec18242ba86
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-06 12:45:28 +0200

    Chore: added cookies provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/materialUI/mtUI.ts";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { CookiesProvider } from "react-cookie";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -17,7 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         clientId={clientId}
         authorizationParams={{ redirect_uri: `${window.location.origin}/home` }}
       >
-        <App />
+        <CookiesProvider>
+          <App />
+        </CookiesProvider>
       </Auth0Provider>
     </ThemeProvider>
   </React.StrictMode>
