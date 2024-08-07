230494a78cce7f0cfacd4aaf9dc5d96092d98cfe
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-07 13:40:05 +0200

    Chore: created an container when i want show the books

---
+++ Frontend/src/theme/materialUI/containerBooks.ts
@@ -0,0 +1,10 @@
+import { SxProps } from "@mui/material";
+import { Theme } from "@mui/material";
+
+export const boxContainerBooks: SxProps<Theme> = {
+  border: "2px solid black",
+  margin: "1rem",
+  marginTop: "0",
+  height: "86%",
+  width: "96.5%",
+};
