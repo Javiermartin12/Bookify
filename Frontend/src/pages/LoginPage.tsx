0fcc5c141fa7e6fd7a9574a28a7a866bd81b8138
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-09 12:36:07 +0200

    Chore: Upgraded login page.

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,42 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Button, Typography } from "@mui/material";
 
 const LoginPage = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <>
+      <Box sx={{ padding: "1rem", height: "100%", width: "100%" }}>
+        <Typography variant="h1" color="aliceblue" gutterBottom>
+          Bookify
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+            padding: "1rem",
+          }}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ margin: "1rem", height: "3rem", width: "7rem" }}
+            onClick={() => loginWithRedirect()}
+          >
+            Log In
+          </Button>
+          <Typography variant="h6" color="aliceblue" gutterBottom>
+            Please log in to continue
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default LoginPage;
