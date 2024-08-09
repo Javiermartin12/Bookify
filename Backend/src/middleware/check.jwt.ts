6dd15c8ac091f51020cb8dd42c702b3a0d84173c
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-09 12:42:31 +0200

    Chore: Code for manage jwt btw front and back.

--- Backend/src/middleware/check.jwt.ts
+++ Backend/src/middleware/check.jwt.ts
@@ -1,8 +1,32 @@
-import config from "../config/config";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwksRsa from "jwks-rsa";
+
+import { app } from "../server";
+
+const jwksUrl = `${process.env.AUTH0_ISSUER}.well-known/jwks.json`;
+
+const jwksClient = jwksRsa({
+  jwksUri: jwksUrl,
+});
+
+export const getKey = (
+  header: { kid: string },
+  callback: (err: Error | null, key?: string | Buffer) => void
+) => {
+  jwksClient.getSigningKey(header.kid, (err, key) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    const signingKey = key?.getPublicKey();
+    callback(null, signingKey);
+  });
+};
 
 export const jwtCheck = auth({
-  audience: config.auth.audience,
-  issuerBaseURL: config.auth.issuer,
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
   tokenSigningAlg: "RS256",
 });
+
+app.use(jwtCheck);
