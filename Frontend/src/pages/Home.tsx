e2de0ae2b1313811c93b6e709e81de6043d753f1
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-09 12:36:57 +0200

    Chore: Manage verify of user.

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RightContainer from "../components/RightContainer";
 import SideBar from "../components/SideBar";
+import { useAuth0 } from "@auth0/auth0-react";
+import axiosInstance from "../axios.config";
 
 export const Home: React.FC = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const createOrFetchUser = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        await axiosInstance.post(
+          "/user",
+          {},
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      } catch (error) {
+        console.error("Error creating/fetching user", error);
+      }
+    };
+    createOrFetchUser();
+  }, [getAccessTokenSilently]);
   return (
     <>
       <SideBar />
