31c357ecbed861dd64782a055caef1b6ed681b4c
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-12 14:53:59 +0200

    Chore: Fixed some styles.

--- Frontend/src/theme/materialUI/containerBooks.ts
+++ Frontend/src/theme/materialUI/containerBooks.ts
@@ -2,9 +2,24 @@ import { SxProps } from "@mui/material";
 import { Theme } from "@mui/material";
 
 export const boxContainerBooks: SxProps<Theme> = {
-  border: "2px solid black",
   margin: "1rem",
-  marginTop: "0",
-  height: "86%",
-  width: "96.5%",
+  borderRadius: "0.5rem",
+  padding: "1rem",
+  width: "93%",
+  zIndex: "-1",
+  marginBottom: "60px",
+  "@media (max-width:430px)": {
+    marginBottom: "100px",
+    width: "85%",
+    height: "100%",
+  },
+};
+
+export const card: SxProps<Theme> = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  cursor: "",
+  height: "100%",
+  width: "100%",
 };
