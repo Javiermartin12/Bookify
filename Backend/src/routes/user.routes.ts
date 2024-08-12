6676fa496efd06b1c02123d56b2da92483960d09
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-12 09:31:58 +0200

    Chore: Created route to register user in backend.

--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -3,11 +3,13 @@ import {
   createUser,
   deleteUser,
   getAllUser,
+  registerUserWithAuth0,
   updateUser,
 } from "../controllers/user.controller";
 
 export const userRoutes = Router();
 userRoutes.get("/", getAllUser);
 userRoutes.post("/", createUser);
+userRoutes.post("/register", registerUserWithAuth0);
 userRoutes.patch("/:userId", updateUser);
 userRoutes.delete("/:userId", deleteUser);
