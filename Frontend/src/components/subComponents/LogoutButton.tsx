7a39a178a92bbd3bef97581642d82a79748277a2
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-12 12:13:27 +0200

    Chore: Relocated button.

--- Frontend/src/components/subComponents/LogoutButton.tsx
+++ Frontend/src/components/subComponents/LogoutButton.tsx
@@ -1,17 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Button } from "@mui/material";
 import React from "react";
 
 const LogoutButton: React.FC = () => {
   const { logout } = useAuth0();
 
   return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: `${window.location.origin}/` } })
-      }
+    <Box
+      sx={{
+        marginTop: "auto",
+        "@media (max-width: 430px)": {
+          display: "none",
+        },
+      }}
     >
-      Log Out
-    </button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() =>
+          logout({ logoutParams: { returnTo: `${window.location.origin}/` } })
+        }
+      >
+        Log Out
+      </Button>
+    </Box>
   );
 };
 
