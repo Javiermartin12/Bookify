ed0b5d38e0c711bcdea65f9087658b7c176b6640
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-13 12:31:17 +0200

    Chore: Created component of settings.

---
+++ Frontend/src/pages/Settings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+
+export const Settings: React.FC = () => {
+  return (
+    <>
+      <div>Settings</div>
+      <SideBar />
+    </>
+  );
+};
