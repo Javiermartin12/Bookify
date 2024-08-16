622fa94cbd6626a91f01a59ef667be3841db664e
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-16 10:37:48 +0200

    Chore: I have changed genre type to string[].

--- Frontend/src/interfaces/booksInterfaces.ts
+++ Frontend/src/interfaces/booksInterfaces.ts
@@ -1,10 +1,8 @@
-import { Genre } from "./genreInterfaces";
-
 export interface BookInterface {
   _id?: string;
   title: string;
   author: string;
-  genre: Genre[];
+  genre: string[];
   publishedDate: Date;
   coverImageUrl: string;
   synopsis: string;
