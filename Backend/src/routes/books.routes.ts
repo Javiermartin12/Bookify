8172b3215810e8e1ccc79ff258a4e35c58a1ca62
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-17 20:03:10 +0200

    Chore: Route from get book by id.

--- Backend/src/routes/books.routes.ts
+++ Backend/src/routes/books.routes.ts
@@ -5,11 +5,13 @@ import {
   updateBooks,
   getAllBooks,
   singupBooksFromFront,
+  getBooksById,
 } from "../controllers/books.controller";
 
 export const booksRoutes = Router();
 
 booksRoutes.get("/", getAllBooks);
+booksRoutes.get("/:id", getBooksById);
 booksRoutes.post("/", createBooks);
 booksRoutes.post("/register", singupBooksFromFront);
 booksRoutes.patch("/:userId", updateBooks);
