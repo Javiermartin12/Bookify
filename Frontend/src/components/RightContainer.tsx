76a509792c5e86201fa2bcf4aeebb8f232747bc4
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-19 23:59:04 +0200

    Chore: Dleted  component search.

--- Frontend/src/components/RightContainer.tsx
+++ Frontend/src/components/RightContainer.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "../CSS/componentsCSS/rightContainer.css";
 import Header from "./Header";
-import SearchBar from "./SearchBar";
 import { ContainerBooks } from "./ContainerBooks";
 
 const RightContainer: React.FC = () => {
   return (
     <div className="rightContainer">
       <Header />
-      <SearchBar />
       <ContainerBooks />
     </div>
   );
