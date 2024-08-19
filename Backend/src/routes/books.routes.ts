0581e66b7d8c865b06746b8daad56dfbd531da67
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-19 22:40:05 +0200

    Chore: Created route by search in data base.

--- Backend/src/routes/books.routes.ts
+++ Backend/src/routes/books.routes.ts
@@ -7,6 +7,7 @@ import {
   singupBooksFromFront,
   getBooksById,
   getBookByUser,
+  searchBooks,
 } from "../controllers/books.controller";
 
 export const booksRoutes = Router();
@@ -14,6 +15,7 @@ export const booksRoutes = Router();
 booksRoutes.get("/", getAllBooks);
 booksRoutes.get("/by/:nameUser", getBookByUser);
 booksRoutes.get("/:id", getBooksById);
+booksRoutes.get("/search/:query", searchBooks);
 booksRoutes.post("/", createBooks);
 
 booksRoutes.post("/register", singupBooksFromFront);
