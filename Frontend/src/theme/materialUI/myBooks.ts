cbbc581061d8103249bcdc2cb4fa6e2b59253bfc
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-19 20:44:58 +0200

    Chore: I have had to modify responsive.

--- Frontend/src/theme/materialUI/myBooks.ts
+++ Frontend/src/theme/materialUI/myBooks.ts
@@ -1,18 +1,23 @@
 import { SxProps } from "@mui/material";
 import { Theme } from "@mui/material";
 
+export const rightMyBookContainer: SxProps<Theme> = {
+  height: "100%",
+  width: "100%",
+};
+
 export const containerBooks: SxProps<Theme> = {
-  margin: "1rem",
-  padding: "1rem",
+  margin: "0.5rem",
+  padding: "0.5rem",
   display: "flex",
   flexDirection: "column",
   flexFlow: "row wrap",
-  height: "60%",
-  width: "93%",
+  height: "70%",
+  width: "95%",
   overflow: "scroll",
   "@media (max-width:430px)": {
     marginBottom: "100px",
-    width: "80%",
+    width: "92%",
     height: "100%",
     display: "flex",
     justifyContent: "center",
@@ -26,11 +31,31 @@ export const cardMyBooks: SxProps<Theme> = {
   cursor: "pointer",
   flexFlow: "row wrpa",
   margin: "0.5rem",
-  height: "auto",
+  height: "55%",
   width: "20%",
   overflow: "hidden",
   "@media (max-width:430px)": {
     width: "60%",
-    height: "55%",
+    height: "32%",
   },
+};
+
+export const headerMyBook: SxProps<Theme> = {
+  "@media (max-width:430px)": {
+    height: "8%",
+    width: "92%",
+    margin: "1rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "@media (min-width:431px)": {
+    height: "20%",
+    width: "95%",
+    margin: "1.3rem",
+  },
+};
+export const title: SxProps<Theme> = {
+  color: "aliceblue",
+  marginTop: "2.2rem",
 };
