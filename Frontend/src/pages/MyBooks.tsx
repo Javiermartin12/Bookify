f53e3694f47bd2891ec63d4665acfd6d4cc8bebf
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-19 12:14:51 +0200

    Chore: Imported new componente header.

--- Frontend/src/pages/MyBooks.tsx
+++ Frontend/src/pages/MyBooks.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SideBar from "../components/SideBar";
+import { HeaderMyBooks } from "../components/HeaderMyBooks";
 
 export const MyBooks: React.FC = () => {
   return (
     <>
-      <div> my books React.FC</div>
       <SideBar />
+      <HeaderMyBooks />
     </>
   );
 };
