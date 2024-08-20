e90b27b878d0cfe9589cbdbc7d6b8519191c40dd
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-20 19:24:38 +0200

    Chore: Styled of the welcome text.

--- Frontend/src/theme/materialUI/loginPage.ts
+++ Frontend/src/theme/materialUI/loginPage.ts
@@ -5,14 +5,21 @@ export const mainBox: SxProps<Theme> = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  justifyContent: "center",
+
   height: "100%",
-  width: "100%",
+  width: "95%",
   padding: "1rem",
 };
 
 export const button: SxProps<Theme> = {
   margin: "1rem",
-  height: "3rem",
-  width: "7rem",
+  height: "7%",
+  width: "9%",
+};
+
+export const boxWelcomePage: SxProps<Theme> = {
+  height: "50%",
+  width: "60%",
+  display: "flex",
+  alignItems: "center",
 };
