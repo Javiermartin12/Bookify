967c6e345f7db45fa1b0d33d872f25d360e2d147
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-21 14:15:32 +0200

    Chore: Modified spacing btw elements.

--- Frontend/src/theme/materialUI/sideBar.ts
+++ Frontend/src/theme/materialUI/sideBar.ts
@@ -1,12 +1,15 @@
 import { SxProps } from "@mui/material";
 import { Theme } from "@mui/material";
 export const myBooksTypogr: SxProps<Theme> = {
+  textDecorationLine: "none",
+
   "@media (max-width:430px)": {
     display: "none",
   },
   "@media (min-width:431px)": {
     color: "aliceblue",
     cursor: "pointer",
+    mt: "1rem",
   },
 };
 
