1a206dbbc98dfaac12b6c1fcddbdd4e64e0273f7
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-21 10:53:39 +0200

    Chore: Modifies responsive and styles.

--- Frontend/src/pages/BooksDetails.tsx
+++ Frontend/src/pages/BooksDetails.tsx
@@ -4,6 +4,7 @@ import { getBooksById } from "../services/bookServices";
 import { BookInterface } from "../interfaces/booksInterfaces";
 import { Alert, Box, Button, CardMedia, Typography } from "@mui/material";
 import {
+  buttonContainer,
   containerBook,
   image,
   mainCpontainer,
@@ -42,37 +43,46 @@ export const BooksDetails: React.FC = () => {
             image={book.coverImageUrl}
             alt={book.title}
           />
-          <Typography variant="h2" gutterBottom>
+          <Typography variant="h4" gutterBottom color="aliceblue">
             {book.title}
           </Typography>
-          <Typography variant="h6" color="textSecondary" gutterBottom>
+          <Typography variant="h5" color="aliceblue" gutterBottom>
             {book.author}
           </Typography>
-          <Typography>
-            Published Date: {new Date(book.publishedDate).toLocaleDateString()}
+          <Typography variant="h6" color="aliceblue">
+            PD: {new Date(book.publishedDate).toLocaleDateString()}
           </Typography>
-          <Typography variant="body2">
+          <Typography variant="h6" color="aliceblue">
             Genre: {book.genre.join(", ")}
           </Typography>
-          <Typography>Published by: {book.nameUser}</Typography>
-          <Typography variant="body1" paragraph>
+          <Typography color="aliceblue" variant="h6">
+            Published by: {book.nameUser}
+          </Typography>
+          <Typography variant="h6" paragraph color="aliceblue">
             {book.synopsis}
           </Typography>
-          <Link href="/home">
-            <Button
-              type="button"
-              variant="contained"
-              color="secondary"
-              fullWidth
-            >
-              Back
-            </Button>
-          </Link>
-          <Link href="">
-            <Button type="button" variant="contained" color="primary" fullWidth>
-              Update
-            </Button>
-          </Link>
+          <Box sx={buttonContainer}>
+            <Link href="/home">
+              <Button
+                type="button"
+                variant="contained"
+                color="secondary"
+                fullWidth
+              >
+                Back
+              </Button>
+            </Link>
+            <Link href="">
+              <Button
+                type="button"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Update
+              </Button>
+            </Link>
+          </Box>
         </Box>
       ) : (
         <Alert severity="warning">No book found.</Alert>
--- Frontend/src/theme/materialUI/bookDetails.ts
+++ Frontend/src/theme/materialUI/bookDetails.ts
@@ -6,13 +6,17 @@ export const mainCpontainer: SxProps<Theme> = {
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-
   padding: "0.5rem",
+  "@media (max-width:430px)": {
+    height: "100%",
+    width: "96%",
+  },
 };
 
 export const containerBook: SxProps<Theme> = {
   width: "60%",
   height: "95%",
+  gap: "1rem",
   maxWidth: "90%",
   margin: "1rem",
   display: "flex",
@@ -23,10 +27,23 @@ export const containerBook: SxProps<Theme> = {
   textAlign: "center",
   padding: "1rem",
   overflowY: "scroll",
+  "@media (max-width:430px)": {
+    width: "90%",
+  },
 };
 export const image: SxProps<Theme> = {
   objectFit: "cover",
   borderRadius: "4px",
   width: "30%",
   height: "100%",
+  "@media (max-width:430px)": {
+    width: "70%",
+    height: "100%",
+  },
+};
+
+export const buttonContainer: SxProps<Theme> = {
+  width: "90%",
+  display: "flex",
+  justifyContent: "space-around",
 };
