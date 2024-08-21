73b240ee88ec8be511f8d0f40fbe4138d9067222
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-21 13:14:45 +0200

    Chore: Changed chat icon for setting icon.

--- Frontend/src/components/SideBar.tsx
+++ Frontend/src/components/SideBar.tsx
@@ -1,11 +1,13 @@
 import "../CSS/componentsCSS/sideBar.css";
 import React from "react";
-import { Home, Message, Search, Add, Book } from "@mui/icons-material";
+import { Home, Search, Add, Book } from "@mui/icons-material";
+import SettingsIcon from "@mui/icons-material/Settings";
+
 import { Box, Typography } from "@mui/material";
 import { sideBarContainerStyles } from "../theme/materialUI/mtUI";
 import { Link } from "wouter";
 import LogoutButton from "./subComponents/LogoutButton";
-import { myBooksTypogr } from "../theme/materialUI/sideBar";
+import { myBooksTypogr, styleLogoutBox } from "../theme/materialUI/sideBar";
 const SideBar: React.FC = () => {
   return (
     <div className="sideBar">
@@ -20,8 +22,8 @@ const SideBar: React.FC = () => {
           <Add />
         </Link>
         <Search />
-        <Link href="/chat">
-          <Message />
+        <Link href="/home">
+          <SettingsIcon />
         </Link>
       </Box>
       <h1 className="sideBarTitle">Home</h1>
@@ -31,7 +33,9 @@ const SideBar: React.FC = () => {
       <Link href="/my-books">
         <Typography sx={myBooksTypogr}>My books</Typography>
       </Link>
-      <LogoutButton />
+      <Box sx={styleLogoutBox}>
+        <LogoutButton />
+      </Box>
     </div>
   );
 };
