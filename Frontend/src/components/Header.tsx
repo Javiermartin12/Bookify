b816fc9f6700f467fa12516cee3fe4397b193c36
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-08-21 13:11:30 +0200

    Chore: Integrated avatar for each user and modify responsive.

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "../CSS/componentsCSS/header.css";
-import { Box, Button, Typography } from "@mui/material";
-import { headerButtonStyles } from "../theme/materialUI/mtUI";
+import { Box, Button, Typography, Avatar } from "@mui/material";
 
 import {
   boxToAvatar,
   boxToRow,
   helloAvatarName,
+  logOut,
 } from "../theme/materialUI/header";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useLocation } from "wouter";
+import { Link } from "wouter";
+import LogoutButton from "./subComponents/LogoutButton";
+import { headerButtonStyles } from "../theme/materialUI/mtUI";
 
 const Header: React.FC = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  const [, navigate] = useLocation();
   if (isLoading) {
     return;
   }
@@ -22,22 +23,25 @@ const Header: React.FC = () => {
     <div className="headerMain">
       <div className="headerContainer">
         <Box sx={boxToRow}>
-          <Box sx={boxToAvatar}></Box>
-          {isAuthenticated && (
-            <Typography sx={helloAvatarName}>Hello, {user?.name}</Typography>
+          {isAuthenticated && user && (
+            <>
+              <Avatar sx={boxToAvatar}>{user.name?.charAt(0)}</Avatar>
+              <Typography sx={helloAvatarName}>Hello, {user?.name}</Typography>
+            </>
           )}
         </Box>
         <h2 className="headerTitle">Books</h2>
         <p className="booksExist"> Number of books exist</p>
       </div>
       <div className="headerContainerRight">
-        <Button
-          variant="contained"
-          sx={headerButtonStyles}
-          onClick={() => navigate("/new-book")}
-        >
-          New book
-        </Button>
+        <Box sx={logOut}>
+          <LogoutButton />
+        </Box>
+        <Link href="/new-book">
+          <Button variant="contained" sx={headerButtonStyles}>
+            New book
+          </Button>
+        </Link>
       </div>
     </div>
   );
